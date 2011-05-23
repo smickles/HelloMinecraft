2ea4c66f542e9ae5a56d8646e7e0de155d5e659e
Michael Carver <xiberphim@gmail.com>
2011-05-23 15:29:16 -0400

    Fix Issue #1
    Remove extra stuff concerning playerListener.
    
    Signed-off-by: Michael Carver <xiberphim@gmail.com>

--- src/me/smickles/HelloMinecraft/HelloMinecraft.java
+++ src/me/smickles/HelloMinecraft/HelloMinecraft.java
@@ -4,9 +4,6 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.event.Event;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -17,13 +14,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HelloMinecraft extends JavaPlugin {
 	
-	private final HelloMinecraftPlayerListener playerListener = new HelloMinecraftPlayerListener(this);
-	
 	Logger log = Logger.getLogger("Minecraft");
 
 	public void onEnable(){ 
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Event.Priority.Normal, this);
 				
 		log.info("Your plugin has been enabled.");
 	}
