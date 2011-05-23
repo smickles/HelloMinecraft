8e880493201133880a520132d17d279b3de9529b
Michael Carver <xiberphim@gmail.com>
2011-05-23 16:07:46 -0400

    Fix Issue #4
    "hello" now sent to all online players via a for loop.
    Gained inspiration from robin0van0der0v's AfkKick.
    http://forums.bukkit.org/threads/misc-afkkick-v1-0-kicks-idling-player
    s-803.18345/
    
    Signed-off-by: Michael Carver <xiberphim@gmail.com>

--- src/me/smickles/HelloMinecraft/HelloMinecraft.java
+++ src/me/smickles/HelloMinecraft/HelloMinecraft.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -30,6 +31,10 @@ public class HelloMinecraft extends JavaPlugin {
 		if(commandLabel.equalsIgnoreCase("hello")){ //If the player typed /hello then say hello
 			
 			log.info("hello");
+			for (Player messageplayer : getServer().getOnlinePlayers()) //for each player online...
+			{
+				    messageplayer.sendMessage("Hello");                 //say hello
+			}
 			return true;
 		}
 		return false;
