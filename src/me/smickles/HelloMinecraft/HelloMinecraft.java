5a57c402b5d2781c63eecd4995eb955a1029eb49
Michael Carver <xiberphim@gmail.com>
2011-05-23 19:02:23 -0400

    Fix Issue #2
    Add repeating task to onEnable(). 
    Loggs the 'hello' event and sends the 'hello' to all online players.
    
    Signed-off-by: Michael Carver <xiberphim@gmail.com>

--- src/me/smickles/HelloMinecraft/HelloMinecraft.java
+++ src/me/smickles/HelloMinecraft/HelloMinecraft.java
@@ -18,6 +18,18 @@ public class HelloMinecraft extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
 
 	public void onEnable(){ 
+		
+		getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() { // on this server's scheduler, run the repeating task of...
+
+		    public void run() {
+		    	log.info("hello (automatic)"); // log the instance of Hello
+		    	for (Player messageplayer : getServer().getOnlinePlayers()) //for each player online...
+				{
+					    messageplayer.sendMessage("Hello");                 //say hello
+				} 
+		    }
+		}, 60L, 18000L); // 60 server ticks / 20 = 3 second delay
+		                 // 18000 server ticks = 15 minute repetition
 				
 		log.info("Your plugin has been enabled.");
 	}
@@ -30,7 +42,7 @@ public class HelloMinecraft extends JavaPlugin {
 		
 		if(commandLabel.equalsIgnoreCase("hello")){ //If the player typed /hello then say hello
 			
-			log.info("hello");
+			log.info("hello (/hello)"); // log the instance of hello
 			for (Player messageplayer : getServer().getOnlinePlayers()) //for each player online...
 			{
 				    messageplayer.sendMessage("Hello");                 //say hello
